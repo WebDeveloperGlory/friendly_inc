85a582ec5145704a39130823fe70457bff80e608
Umuerri Glory <104975242+WebDeveloperGlory@users.noreply.github.com>
2025-04-08 16:31:50 +0100

    Update authRoutes.js

--- app/routes/authRoutes.js
+++ app/routes/authRoutes.js
@@ -9,7 +9,8 @@ router.post( '/complete-signup', controller.completeEnrollment );
 router.post( '/login/user', controller.loginUser );
 router.post( '/login/admin', controller.adminLogin );
 router.post( '/login/rider', controller.riderLogin );
-router.post( '/complete-password-reset', authenticateUser, controller.completePasswordReset);
+router.post( 'initiate-password-reset', controller.initiatePasswordReset)
+router.post( '/complete-password-reset', controller.completePasswordReset);
 router.post( '/change-password', authenticateUser, controller.changePassword );
 router.post( '/generate-otp', authenticateUser, controller.resendOtp );
 // router.post( '/register/seller', controller.registerSeller );
