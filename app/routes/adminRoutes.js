d61cc85ae6e6a730ee7c90390883a9fb25c30b5d
Umuerri Glory <104975242+WebDeveloperGlory@users.noreply.github.com>
2025-04-08 19:03:15 +0100

    Update adminRoutes.js

--- app/routes/adminRoutes.js
+++ app/routes/adminRoutes.js
@@ -6,9 +6,9 @@ const { hasAdminPermissions } = require('../middlewares/adminMiddlewares');
 const router = Router();
 
 router.get( '/dashboard', authenticateUser, hasAdminPermissions, controller.getAdminDashboardDetails);
-router.get( '/profile', hasAdminPermissions, authenticateUser, controller.getPersonalDetails );
+router.get( '/profile', authenticateUser, hasAdminPermissions, controller.getPersonalDetails );
 router.put( '/profile', authenticateUser, hasAdminPermissions, controller.editPersonalDetails );
-router.post( '/register/staff', hasAdminPermissions, authenticateUser, controller.registerAdministratorOrRider );
+router.post( '/register/staff', authenticateUser, hasAdminPermissions, controller.registerAdministratorOrRider );
 router.post( '/assign-rider/:orderId', authenticateUser, hasAdminPermissions, controller.assignRidersToOrders );
 
 module.exports = router;
