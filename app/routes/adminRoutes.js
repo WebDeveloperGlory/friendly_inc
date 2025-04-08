10203ba80a222d1e2d1bb8b1487fe6e385c60025
Umuerri Glory <104975242+WebDeveloperGlory@users.noreply.github.com>
2025-04-08 19:01:03 +0100

    Update adminRoutes.js

--- app/routes/adminRoutes.js
+++ app/routes/adminRoutes.js
@@ -1,13 +1,14 @@
 const { Router } = require('express');
 const controller = require('../controllers/adminController');
 const { authenticateUser } = require('../middlewares/authMiddlewares');
+const { hasAdminPermissions } = require('../middlewares/adminMiddlewares');
 
 const router = Router();
 
-router.get( '/dashboard', authenticateUser, controller.getAdminDashboardDetails);
-router.get( '/profile', authenticateUser, controller.getPersonalDetails );
-router.put( '/profile', authenticateUser, controller.editPersonalDetails );
-router.post( '/register/staff', authenticateUser, controller.registerAdministratorOrRider );
-router.post( '/assign-rider/:orderId', authenticateUser, controller.assignRidersToOrders );
+router.get( '/dashboard', authenticateUser, hasAdminPermissions, controller.getAdminDashboardDetails);
+router.get( '/profile', hasAdminPermissions, authenticateUser, controller.getPersonalDetails );
+router.put( '/profile', authenticateUser, hasAdminPermissions, controller.editPersonalDetails );
+router.post( '/register/staff', hasAdminPermissions, authenticateUser, controller.registerAdministratorOrRider );
+router.post( '/assign-rider/:orderId', authenticateUser, hasAdminPermissions, controller.assignRidersToOrders );
 
 module.exports = router;
