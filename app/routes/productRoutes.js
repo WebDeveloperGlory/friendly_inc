5af119baf2db14b2ae6e4bceab25b52b9aecb0ba
Umuerri Glory <104975242+WebDeveloperGlory@users.noreply.github.com>
2025-04-09 16:24:24 +0100

    Update productRoutes.js

--- app/routes/productRoutes.js
+++ app/routes/productRoutes.js
@@ -1,14 +1,15 @@
 const { Router } = require('express');
 const controller = require('../controllers/productController');
 const { authenticateUser } = require('../middlewares/authMiddlewares');
+const { hasAdminPermissions } = require('../middlewares/adminMiddlewares');
 const imageService = require('../services/imageService');
 
 const router = Router();
 
 router.get('/', controller.getAllProducts);
-router.post('/', controller.createProduct);
+router.post('/', authenticateUser, hasAdminPermissions, controller.createProduct);
 router.get('/:productId', controller.getSingleProduct);
-router.post('/:productId/update', imageService.uploadProductImage, controller.updateProduct);
-router.delete('/:productId/delete', controller.deleteProduct);
+router.post('/:productId/update',authenticateUser, hasAdminPermissions, imageService.uploadProductImage, controller.updateProduct);
+router.delete('/:productId/delete', authenticateUser, hasAdminPermissions, controller.deleteProduct);
 
 module.exports = router;
