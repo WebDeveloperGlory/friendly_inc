fcce64fd2cc2a90c2792d95a9d53ade71c993500
Umuerri Glory <104975242+WebDeveloperGlory@users.noreply.github.com>
2025-04-09 19:50:05 +0100

    Update user routes

---
+++ app/routes/userRoutes.js
@@ -0,0 +1,20 @@
+const { Router } = require('express');
+const controller = require('../controllers/userController');
+const { authenticateUser } = require('../middlewares/authMiddlewares');
+
+const router = Router();
+
+// router.get( '/profile', authenticateUser, controller.getPersonalDetails );
+
+router.get( '/favorites', authenticateUser,  controller.getUserFavorites );
+router.post( '/favorites/add', authenticateUser, controller.addProductToFavorites );
+router.post( '/favorites/remove', authenticateUser, controller.removeProductFromFavorites );
+
+router.get( '/addresses', authenticateUser,  controller.getUserAddresses );
+router.post( '/addresses/add', authenticateUser, controller.addAddress );
+
+router.get( '/cart', authenticateUser,  controller.getUserCart );
+router.post( '/cart/add', authenticateUser, controller.addProductToCart );
+router.post( '/cart/remove', authenticateUser, controller.deleteProductFromCart );
+
+module.exports = router;
