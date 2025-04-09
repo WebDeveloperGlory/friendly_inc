8b6cbc342081a37189d8086cffea8d720e13236a
Umuerri Glory <104975242+WebDeveloperGlory@users.noreply.github.com>
2025-04-09 19:09:24 +0100

    Update userController.js

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -14,4 +14,95 @@ exports.getProfile = async ( req, res ) => {
     }
 }
 
+exports.getProfile = async ( req, res ) => {
+    try {
+        const result = await userService.getProfile( req.user );
+
+        if( result.success ) {
+            return success( res, result.message, result.data );
+        }
+        return error( res, result.message );
+    } catch ( err ) {
+        return serverError( res, err );
+    }
+}
+
+exports.getProfile = async ( req, res ) => {
+    try {
+        const result = await userService.getProfile( req.user );
+
+        if( result.success ) {
+            return success( res, result.message, result.data );
+        }
+        return error( res, result.message );
+    } catch ( err ) {
+        return serverError( res, err );
+    }
+}
+
+exports.getProfile = async ( req, res ) => {
+    try {
+        const result = await userService.getProfile( req.user );
+
+        if( result.success ) {
+            return success( res, result.message, result.data );
+        }
+        return error( res, result.message );
+    } catch ( err ) {
+        return serverError( res, err );
+    }
+}
+
+exports.getProfile = async ( req, res ) => {
+    try {
+        const result = await userService.getProfile( req.user );
+
+        if( result.success ) {
+            return success( res, result.message, result.data );
+        }
+        return error( res, result.message );
+    } catch ( err ) {
+        return serverError( res, err );
+    }
+}
+
+exports.getProfile = async ( req, res ) => {
+    try {
+        const result = await userService.getProfile( req.user );
+
+        if( result.success ) {
+            return success( res, result.message, result.data );
+        }
+        return error( res, result.message );
+    } catch ( err ) {
+        return serverError( res, err );
+    }
+}
+
+exports.getProfile = async ( req, res ) => {
+    try {
+        const result = await userService.getProfile( req.user );
+
+        if( result.success ) {
+            return success( res, result.message, result.data );
+        }
+        return error( res, result.message );
+    } catch ( err ) {
+        return serverError( res, err );
+    }
+}
+
+exports.getProfile = async ( req, res ) => {
+    try {
+        const result = await userService.getProfile( req.user );
+
+        if( result.success ) {
+            return success( res, result.message, result.data );
+        }
+        return error( res, result.message );
+    } catch ( err ) {
+        return serverError( res, err );
+    }
+}
+
 module.exports = exports;
