4228ecaf5e317f0c1d782c3c5c8174cb1714d23b
Umuerri Glory <104975242+WebDeveloperGlory@users.noreply.github.com>
2025-04-10 20:19:29 +0100

    Update orderRoutes.js

--- app/routes/orderRoutes.js
+++ app/routes/orderRoutes.js
@@ -1,11 +1,12 @@
 const { Router } = require('express');
 const controller = require('../controllers/orderController');
 const { authenticateUser } = require('../middlewares/authMiddlewares');
+const { hasAdminPermissions } = require('../middlewares/adminMiddlewares');
 
 const router = Router();
 
-router.get( '/', authenticateUser, controller.getAllOrders);
+router.get( '/', authenticateUser, hasAdminPermissions, controller.getAllOrders);
 router.get( '/:orderId', authenticateUser, controller.getOneOrder );
-router.put( '/:orderId/status', authenticateUser, controller.updateOrderStatus );
+router.put( '/:orderId/status', authenticateUser, hasAdminPermissions, controller.updateOrderStatus );
 
 module.exports = router;
