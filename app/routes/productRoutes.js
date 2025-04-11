c0edf9644f834cd2b95aa1e74d5faa479b695e66
Umuerri Glory <104975242+WebDeveloperGlory@users.noreply.github.com>
2025-04-11 21:13:48 +0100

    Update productRoutes.js

--- app/routes/productRoutes.js
+++ app/routes/productRoutes.js
@@ -8,7 +8,6 @@ const router = Router();
 
 router.get('/', controller.getAllProducts);
 router.post('/', authenticateUser, hasAdminPermissions, controller.createProduct);
-router.get('/category/:categoryId', controller.getProductsByCategory);
 router.get('/:productId', controller.getSingleProduct);
 router.post('/:productId/update',authenticateUser, hasAdminPermissions, imageService.uploadProductImage, controller.updateProduct);
 router.delete('/:productId/delete', authenticateUser, hasAdminPermissions, controller.deleteProduct);
