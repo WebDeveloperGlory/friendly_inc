8900d35af965e651c9de19e51ade98e25bea6d0c
Umuerri Glory <104975242+WebDeveloperGlory@users.noreply.github.com>
2025-04-14 01:14:45 +0100

    Update env.js

--- app/config/env.js
+++ app/config/env.js
@@ -8,4 +8,7 @@ module.exports = {
     ALLOWED_ORIGINS: process.env.ALLOWED_ORIGINS,
     EMAIL_USER: process.env.EMAIL_USER,
     EMAIL_PASS: process.env.EMAIL_PASS,
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
 }
