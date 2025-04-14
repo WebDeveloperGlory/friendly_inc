6f71ffc67884195dd66e13473414077dc0d792ee
Umuerri Glory <104975242+WebDeveloperGlory@users.noreply.github.com>
2025-04-14 01:23:34 +0100

    Update alt product routes

---
+++ app/routes/altProductRoutes.js
@@ -0,0 +1,16 @@
+const { Router } = require('express');
+const controller = require('../controllers/altProductController');
+const { authenticateUser } = require('../middlewares/authMiddlewares');
+const { hasAdminPermissions } = require('../middlewares/adminMiddlewares');
+
+const router = Router();
+
+router.get('/', controller.getAllProducts);
+router.post('/', authenticateUser, hasAdminPermissions, controller.createProduct);
+router.get('/:productId', controller.getSingleProduct);
+router.post('/:productId/update', authenticateUser, hasAdminPermissions, controller.updateProduct);
+router.delete('/:productId/delete', authenticateUser, hasAdminPermissions, controller.deleteProduct);
+router.put('/:productId/quantity', authenticateUser, hasAdminPermissions, controller.updateProductQuantity);
+router.put('/:productId/status', authenticateUser, hasAdminPermissions, controller.updateProductStatus);
+
+module.exports = router;
